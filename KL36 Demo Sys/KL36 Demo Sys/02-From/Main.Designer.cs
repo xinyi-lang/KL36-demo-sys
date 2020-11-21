06e7557ee31952bec334d344df15ac918eddc48c
moraKX <1638800731@qq.com>
2020-11-21 16:14:26 +0800

    重命名窗体与设计了菜单栏

---
+++ KL36 Demo Sys/KL36 Demo Sys/02-From/Main.Designer.cs
@@ -0,0 +1,345 @@
+﻿namespace KL36_Demo_Sys
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.开始ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.实验一ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.实验简介ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.演示视频ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.开始实验ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.实验二ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.帮助ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.实验简介ToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.演示视频ToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.开始实验ToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem4 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem5 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem6 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem7 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem8 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem9 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem10 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem11 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem12 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem13 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem14 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem15 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem16 = new System.Windows.Forms.ToolStripMenuItem();
+            this.返回首页ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.退出ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.系统演示系统ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.系统手册ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.开始ToolStripMenuItem,
+            this.实验一ToolStripMenuItem,
+            this.实验二ToolStripMenuItem,
+            this.toolStripMenuItem5,
+            this.toolStripMenuItem13,
+            this.toolStripMenuItem9,
+            this.toolStripMenuItem1,
+            this.帮助ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1182, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 开始ToolStripMenuItem
+            // 
+            this.开始ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.返回首页ToolStripMenuItem,
+            this.退出ToolStripMenuItem});
+            this.开始ToolStripMenuItem.Name = "开始ToolStripMenuItem";
+            this.开始ToolStripMenuItem.Size = new System.Drawing.Size(51, 24);
+            this.开始ToolStripMenuItem.Text = "开始";
+            // 
+            // 实验一ToolStripMenuItem
+            // 
+            this.实验一ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.实验简介ToolStripMenuItem,
+            this.演示视频ToolStripMenuItem,
+            this.开始实验ToolStripMenuItem});
+            this.实验一ToolStripMenuItem.Name = "实验一ToolStripMenuItem";
+            this.实验一ToolStripMenuItem.Size = new System.Drawing.Size(66, 24);
+            this.实验一ToolStripMenuItem.Text = "实验一";
+            // 
+            // 实验简介ToolStripMenuItem
+            // 
+            this.实验简介ToolStripMenuItem.Name = "实验简介ToolStripMenuItem";
+            this.实验简介ToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.实验简介ToolStripMenuItem.Text = "实验简介";
+            // 
+            // 演示视频ToolStripMenuItem
+            // 
+            this.演示视频ToolStripMenuItem.Name = "演示视频ToolStripMenuItem";
+            this.演示视频ToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.演示视频ToolStripMenuItem.Text = "演示视频";
+            // 
+            // 开始实验ToolStripMenuItem
+            // 
+            this.开始实验ToolStripMenuItem.Name = "开始实验ToolStripMenuItem";
+            this.开始实验ToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.开始实验ToolStripMenuItem.Text = "开始实验";
+            // 
+            // 实验二ToolStripMenuItem
+            // 
+            this.实验二ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.实验简介ToolStripMenuItem1,
+            this.演示视频ToolStripMenuItem1,
+            this.开始实验ToolStripMenuItem1});
+            this.实验二ToolStripMenuItem.Name = "实验二ToolStripMenuItem";
+            this.实验二ToolStripMenuItem.Size = new System.Drawing.Size(66, 24);
+            this.实验二ToolStripMenuItem.Text = "实验二";
+            // 
+            // 帮助ToolStripMenuItem
+            // 
+            this.帮助ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.系统演示系统ToolStripMenuItem,
+            this.系统手册ToolStripMenuItem});
+            this.帮助ToolStripMenuItem.Name = "帮助ToolStripMenuItem";
+            this.帮助ToolStripMenuItem.Size = new System.Drawing.Size(51, 24);
+            this.帮助ToolStripMenuItem.Text = "帮助";
+            // 
+            // 实验简介ToolStripMenuItem1
+            // 
+            this.实验简介ToolStripMenuItem1.Name = "实验简介ToolStripMenuItem1";
+            this.实验简介ToolStripMenuItem1.Size = new System.Drawing.Size(216, 26);
+            this.实验简介ToolStripMenuItem1.Text = "实验简介";
+            // 
+            // 演示视频ToolStripMenuItem1
+            // 
+            this.演示视频ToolStripMenuItem1.Name = "演示视频ToolStripMenuItem1";
+            this.演示视频ToolStripMenuItem1.Size = new System.Drawing.Size(216, 26);
+            this.演示视频ToolStripMenuItem1.Text = "演示视频";
+            // 
+            // 开始实验ToolStripMenuItem1
+            // 
+            this.开始实验ToolStripMenuItem1.Name = "开始实验ToolStripMenuItem1";
+            this.开始实验ToolStripMenuItem1.Size = new System.Drawing.Size(216, 26);
+            this.开始实验ToolStripMenuItem1.Text = "开始实验";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem2,
+            this.toolStripMenuItem3,
+            this.toolStripMenuItem4});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(66, 24);
+            this.toolStripMenuItem1.Text = "实验六";
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(216, 26);
+            this.toolStripMenuItem2.Text = "实验简介";
+            // 
+            // toolStripMenuItem3
+            // 
+            this.toolStripMenuItem3.Name = "toolStripMenuItem3";
+            this.toolStripMenuItem3.Size = new System.Drawing.Size(216, 26);
+            this.toolStripMenuItem3.Text = "演示视频";
+            // 
+            // toolStripMenuItem4
+            // 
+            this.toolStripMenuItem4.Name = "toolStripMenuItem4";
+            this.toolStripMenuItem4.Size = new System.Drawing.Size(216, 26);
+            this.toolStripMenuItem4.Text = "开始实验";
+            // 
+            // toolStripMenuItem5
+            // 
+            this.toolStripMenuItem5.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem6,
+            this.toolStripMenuItem7,
+            this.toolStripMenuItem8});
+            this.toolStripMenuItem5.Name = "toolStripMenuItem5";
+            this.toolStripMenuItem5.Size = new System.Drawing.Size(66, 24);
+            this.toolStripMenuItem5.Text = "实验三";
+            // 
+            // toolStripMenuItem6
+            // 
+            this.toolStripMenuItem6.Name = "toolStripMenuItem6";
+            this.toolStripMenuItem6.Size = new System.Drawing.Size(216, 26);
+            this.toolStripMenuItem6.Text = "实验简介";
+            // 
+            // toolStripMenuItem7
+            // 
+            this.toolStripMenuItem7.Name = "toolStripMenuItem7";
+            this.toolStripMenuItem7.Size = new System.Drawing.Size(216, 26);
+            this.toolStripMenuItem7.Text = "演示视频";
+            // 
+            // toolStripMenuItem8
+            // 
+            this.toolStripMenuItem8.Name = "toolStripMenuItem8";
+            this.toolStripMenuItem8.Size = new System.Drawing.Size(216, 26);
+            this.toolStripMenuItem8.Text = "开始实验";
+            // 
+            // toolStripMenuItem9
+            // 
+            this.toolStripMenuItem9.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem10,
+            this.toolStripMenuItem11,
+            this.toolStripMenuItem12});
+            this.toolStripMenuItem9.Name = "toolStripMenuItem9";
+            this.toolStripMenuItem9.Size = new System.Drawing.Size(66, 24);
+            this.toolStripMenuItem9.Text = "实验五";
+            // 
+            // toolStripMenuItem10
+            // 
+            this.toolStripMenuItem10.Name = "toolStripMenuItem10";
+            this.toolStripMenuItem10.Size = new System.Drawing.Size(216, 26);
+            this.toolStripMenuItem10.Text = "实验简介";
+            // 
+            // toolStripMenuItem11
+            // 
+            this.toolStripMenuItem11.Name = "toolStripMenuItem11";
+            this.toolStripMenuItem11.Size = new System.Drawing.Size(216, 26);
+            this.toolStripMenuItem11.Text = "演示视频";
+            // 
+            // toolStripMenuItem12
+            // 
+            this.toolStripMenuItem12.Name = "toolStripMenuItem12";
+            this.toolStripMenuItem12.Size = new System.Drawing.Size(216, 26);
+            this.toolStripMenuItem12.Text = "开始实验";
+            // 
+            // toolStripMenuItem13
+            // 
+            this.toolStripMenuItem13.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem14,
+            this.toolStripMenuItem15,
+            this.toolStripMenuItem16});
+            this.toolStripMenuItem13.Name = "toolStripMenuItem13";
+            this.toolStripMenuItem13.Size = new System.Drawing.Size(66, 24);
+            this.toolStripMenuItem13.Text = "实验四";
+            // 
+            // toolStripMenuItem14
+            // 
+            this.toolStripMenuItem14.Name = "toolStripMenuItem14";
+            this.toolStripMenuItem14.Size = new System.Drawing.Size(216, 26);
+            this.toolStripMenuItem14.Text = "实验简介";
+            // 
+            // toolStripMenuItem15
+            // 
+            this.toolStripMenuItem15.Name = "toolStripMenuItem15";
+            this.toolStripMenuItem15.Size = new System.Drawing.Size(216, 26);
+            this.toolStripMenuItem15.Text = "演示视频";
+            // 
+            // toolStripMenuItem16
+            // 
+            this.toolStripMenuItem16.Name = "toolStripMenuItem16";
+            this.toolStripMenuItem16.Size = new System.Drawing.Size(216, 26);
+            this.toolStripMenuItem16.Text = "开始实验";
+            // 
+            // 返回首页ToolStripMenuItem
+            // 
+            this.返回首页ToolStripMenuItem.Name = "返回首页ToolStripMenuItem";
+            this.返回首页ToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.返回首页ToolStripMenuItem.Text = "返回首页";
+            // 
+            // 退出ToolStripMenuItem
+            // 
+            this.退出ToolStripMenuItem.Name = "退出ToolStripMenuItem";
+            this.退出ToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.退出ToolStripMenuItem.Text = "退出";
+            this.退出ToolStripMenuItem.Click += new System.EventHandler(this.退出ToolStripMenuItem_Click);
+            // 
+            // 系统演示系统ToolStripMenuItem
+            // 
+            this.系统演示系统ToolStripMenuItem.Name = "系统演示系统ToolStripMenuItem";
+            this.系统演示系统ToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.系统演示系统ToolStripMenuItem.Text = "使用视频";
+            // 
+            // 系统手册ToolStripMenuItem
+            // 
+            this.系统手册ToolStripMenuItem.Name = "系统手册ToolStripMenuItem";
+            this.系统手册ToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.系统手册ToolStripMenuItem.Text = "系统手册";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1182, 753);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "嵌入式实验演示系统  --基于KL36";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 开始ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 返回首页ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 退出ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 实验一ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 实验简介ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 演示视频ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 开始实验ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 实验二ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 实验简介ToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem 演示视频ToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem 开始实验ToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem5;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem6;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem7;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem8;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem13;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem14;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem15;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem16;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem9;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem10;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem11;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem12;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem3;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem4;
+        private System.Windows.Forms.ToolStripMenuItem 帮助ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 系统演示系统ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 系统手册ToolStripMenuItem;
+    }
+}
+
--- KL36 Demo Sys/KL36 Demo Sys/02-From/Main.cs
+++ KL36 Demo Sys/KL36 Demo Sys/02-From/Main.cs
@@ -16,5 +16,14 @@ namespace KL36_Demo_Sys
         {
             InitializeComponent();
         }
+
+        private void 退出ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string msg = "确定需要退出吗？";
+            if ((int)MessageBox.Show(msg, "提示", MessageBoxButtons.OKCancel, MessageBoxIcon.Exclamation) == 1)
+            {
+                this.Dispose();
+            }
+        }
     }
 }
